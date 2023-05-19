0b12b3fb12d34ea2e2f59899216ac7cedfa36aa1
thoughtsunificator <oss@thoughtsunificator.me>
2023-05-19 19:51:57 +0200

    Clean up

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,9 @@
-import path from "path"
 import postcss from "rollup-plugin-postcss"
 import postcssImport from "postcss-import"
 import postcssEasyImport  from "postcss-easy-import"
-import { terser } from "rollup-plugin-terser"
+import { terser } from "@wwa/rollup-plugin-terser"
 import copy from "rollup-plugin-copy-watch"
-import { nodeResolve } from "@rollup/plugin-node-resolve"
+import nodeResolve from "@rollup/plugin-node-resolve"
 import serve from "rollup-plugin-serve"
 import livereload from "rollup-plugin-livereload"
 import rollupWindowEnv from "@thoughtsunificator/rollup-plugin-window-env"
@@ -34,10 +33,10 @@ export default {
 			flatten: false
 		}),
 		nodeResolve(),
+		isDevelopment && livereload("dist"),
 		isDevelopment && serve({
 			contentBase: "dist",
-			port: 3000
+			port: process.env.PORT || 3000
 		}),
-		isDevelopment && livereload("dist")
 	]
 }
