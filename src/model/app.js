cce3ab2291578900703a327d73bd446185763a57
thoughtsunificator <oss@thoughtsunificator.me>
2024-12-15 06:30:47 +0100

    Update to domodel@2.2.0

--- src/model/app.js
+++ src/model/app.js
@@ -1,6 +1,24 @@
-export { default as AppBinding } from "./app.binding.js"
+import { Binding } from "domodel"
 
-export default {
+const AppModel = {
 	tagName: "div",
 	id: "app"
 }
+
+class AppBinding extends Binding {
+
+	/**
+	 * @param {App} app
+	 */
+	constructor(app) {
+		super()
+		this.app = app
+	}
+
+	onCreated() {
+		this.root.textContent = this.app.message
+	}
+
+}
+
+export { AppModel, AppBinding }
